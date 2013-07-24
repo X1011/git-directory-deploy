376df79ab7f6d59003933baf6db3d331d7547d90
X1011 <XX1011@gmail.com>
2013-07-24 06:34:14 +0000

    default user identity for deploy script,
    commit deploy directory when there *are* changes, not when there are no changes (>_<)

--- deploy.sh
+++ deploy.sh
@@ -4,8 +4,21 @@ set -e #abort if any command fails
 deploy_directory=dist
 deploy_branch=gh-pages
 
+#if no user identity is already set in the current git environment, use this:
+default_username=deploy.sh
+default_email=XX1011+$default_user@gmail.com
+
 commit_title=`git log -n 1 --format="%s" HEAD`
 commit_hash=`git log -n 1 --format="%H" HEAD`
+
+set_user_id() {
+	if [[ -z `git config user.name` ]]; then
+		git config user.name "$default_username"
+	fi
+	if [[ -z `git config user.email` ]]; then
+		git config user.email "$default_email"
+	fi
+}
 
 previous_branch=`git rev-parse --abbrev-ref HEAD`
 if [[ $previous_branch = "HEAD" ]]; then
@@ -18,15 +31,16 @@ if ! ( git diff --exit-code --quiet \
 	exit 1
 fi
 
-git --work-tree $deploy_directory checkout $deploy_branch --force
-git --work-tree $deploy_directory add --all
+git --work-tree "$deploy_directory" checkout $deploy_branch --force
+git --work-tree "$deploy_directory" add --all
 
-if git --work-tree $deploy_directory diff --exit-code --quiet HEAD; then
-	git --work-tree $deploy_directory commit -m \
+if git --work-tree "$deploy_directory" diff --exit-code --quiet HEAD; then
+	echo No changes to files in $deploy_directory. Skipping commit.
+else
+	set_user_id
+	git --work-tree "$deploy_directory" commit -m \
 		"publish: $commit_title"$'\n\n'"generated from commit $commit_hash"
 	git push origin $deploy_branch
-else
-	echo No changes to files in $deploy_directory. Skipping commit.
 fi
 
 git checkout $previous_branch --force
