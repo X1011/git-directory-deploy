a68965d16385b6354716172559011f4da70a040c
X1011 <XX1011@gmail.com>
2013-07-25 06:28:51 +0000

    specify local branch to fetch to

--- deploy.sh
+++ deploy.sh
@@ -32,7 +32,7 @@ if ! git diff --exit-code --quiet --cached; then
 	exit 1
 fi
 
-git fetch origin $deploy_branch
+git fetch origin $deploy_branch:$deploy_branch
 
 #make deploy_branch the current branch
 git symbolic-ref HEAD refs/heads/$deploy_branch
